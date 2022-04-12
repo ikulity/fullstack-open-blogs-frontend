31daa403541c1742dab1fab73d397a3d760acf08
Iikka <t8kuii00@students.oamk.fi>
2022-04-12 20:07:08 +0300

    Exercise 5.7 - add detailed versions of blogs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,29 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+    const [showDetails, setShowDetails] = useState(false)
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+    if (showDetails) return (
+        <div style={blogStyle}>
+            {blog.title} - {blog.author} <button onClick={() => setShowDetails(false)}>hide</button>
+            <p>{blog.url}</p>
+            <p>likes {blog.likes} <button>like</button></p>
+            <p>{blog.user.name}</p>
+        </div>
+
+    )
+    return (
+        <div style={blogStyle}>
+            {blog.title} - {blog.author} <button onClick={() => setShowDetails(true)}>view</button>
+        </div>
+    )
+}
 
 export default Blog
