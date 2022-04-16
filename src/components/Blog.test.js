0cf27f3d84995f07c47c0b1ab6cb8d36ea5755b1
Iikka <t8kuii00@students.oamk.fi>
2022-04-16 12:52:06 +0300

    Exercise 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
-    const blog = {
-        title: 'test title',
-        url: 'www.test.com',
-        likes: 0,
-        author: 'tester dude'
+const blog = {
+    title: 'test title',
+    url: 'www.test.com',
+    likes: 0,
+    author: 'tester dude',
+    user: {
+        username: 'trollAccount',
+        name: 'myname'
     }
+}
 
+test('renders default content', () => {
     const { container } = render(<Blog blog={blog} />)
-    screen.debug()
+
     const detailsButton = container.querySelector('#detailsButton')
     expect(detailsButton).toHaveTextContent('view')
 
@@ -24,4 +29,23 @@ test('renders content', () => {
 
     const likesElement = container.querySelector('#likes')
     expect(likesElement).toBeNull()
+})
+
+test('renders detailed content when pressing view button', () => {
+    const { container } = render(<Blog blog={blog} />)
+
+    const button = screen.getByText('view')
+    userEvent.click(button)
+
+    const detailsButton = container.querySelector('#detailsButton')
+    expect(detailsButton).toHaveTextContent('hide')
+
+    const likeButton = container.querySelector('#likeButton')
+    expect(likeButton).toHaveTextContent('like')
+
+    const urlElement = container.querySelector('#url')
+    expect(urlElement).toHaveTextContent(blog.url)
+
+    const likesElement = container.querySelector('#likes')
+    expect(likesElement).toHaveTextContent(`likes ${blog.likes}`)
 })
